d9de1a792aaece319549b20918c1501355681538
Tharanes <39825015+Tharanes@users.noreply.github.com>
2018-09-01 19:27:55 +0530

    Update register.php

--- register.php
+++ register.php
@@ -6,6 +6,7 @@
 	$email = $_POST['email'];
 	$stmt = $db->prepare("INSERT INTO register (email) VALUES (?)");
 	$stmt->bind_param('s',$email);
+	$stmt->execute();
 	 // entering 
 	$sql = "SELECT * FROM register";
 	$res = mysqli_query($db,$sql);
@@ -15,7 +16,7 @@
 	}
 	$filename = fopen('log.json','w');
 	fwrite($filename,json_encode($arr));
-	$stmt->execute();
+	
 	$db->close();
 	header('Location: index.html');
 ?>
